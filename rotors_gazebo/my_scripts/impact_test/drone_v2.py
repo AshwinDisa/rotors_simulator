d0a18c3179ec720084deffee229248b96490ffd8
AshwinDisa <ashwin.disa@gmail.com>
2023-01-10 16:59:25 +0530

    just positions

--- rotors_gazebo/my_scripts/impact_test/drone_v2.py
+++ rotors_gazebo/my_scripts/impact_test/drone_v2.py
@@ -137,10 +137,10 @@ if __name__ == '__main__':
 
         rospy.init_node('drone_node')
 
-        hover_position = [0, 0, 3]
+        hover_position = [1, 1, 8]
 
         traj_initial_pos = np.array(hover_position)
-        traj_final_pos = np. array([8, 8, 3])
+        traj_final_pos = np. array([20, 4, 6])
 
         Xstart = np.zeros((4, 4))
         Xend = np.zeros((4, 4))
@@ -148,7 +148,7 @@ if __name__ == '__main__':
         Xstart[:3, 3] = traj_initial_pos
         Xend[:3, 3] = traj_final_pos
 
-        Tf = 13                                  # time to reach from start to end position
+        Tf = 12                                # time to reach from start to end position
         samplingTime = 1/100                    # sampling time in seconds
         N = int(Tf/samplingTime)                # number of samples
         method = 5                              # interpolation method
